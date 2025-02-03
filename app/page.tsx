83f9c4c056263368ca2665cb36624b3373e5b2e1
meccacodes <hi@windfallyarn.com>
2025-02-03 12:09:47 -0800

    AG-3 testing Jira connection

--- app/page.tsx
+++ app/page.tsx
@@ -14,9 +14,7 @@ export default function Home() {
           priority
         />
         <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
+          <li>test, test, test</li>
           <li>Save and see your changes instantly.</li>
         </ol>
 
