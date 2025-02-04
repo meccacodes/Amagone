5f278ee6df1d0b546d1a88bf81199cdc784b3905
meccacodes <hi@windfallyarn.com>
2025-02-04 11:15:18 -0800

    Added some Amazon-like styling

--- app/page.tsx
+++ app/page.tsx
@@ -19,12 +19,10 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <div className={styles.logoContainer}>
-        <h1>amagon</h1>
+        <h1>amagone</h1>
         <h2>search outside the box</h2>
       </div>
       <input className={styles.searchInput} type="text" />
-      {/* Which would be better here, an input or a button? */}
-      <input className={styles.searchSubmit} type="submit" />
       <button className={styles.searchButton}>Search</button>
       {/* Should this be a component? */}
       <div className={styles.results}>
